d71e67bd4c32f84ceb19e235eb16b526f12c73b2
audreyleighleigh <84555172+audreyleighleigh@users.noreply.github.com>
2023-09-09 13:02:44 -0600

    Add files via upload

--- Visualization.js
+++ Visualization.js
@@ -4,37 +4,37 @@
 function animateClouds() {
     const cloud1 = document.getElementById('Cloud1');
     const cloud2 = document.getElementById('Cloud2');
-    
+
     // Initial positions of the clouds
     let cloud1X = 1920; // Start off-screen to the right
     let cloud2X = 1920; // Start off-screen to the right
-    
+
     // Animation speed (you can adjust this value)
-    const cloudSpeed = 5; // Adjust the speed as needed
-    
+    const cloudSpeed = 2; // Adjust the speed as needed
+
     // Animation function
     function moveClouds() {
         // Move clouds to the left
         cloud1X -= cloudSpeed;
         cloud2X -= cloudSpeed;
-        
+
         // Apply new positions
-        cloud1.setAttribute('transform', `translate(${cloud1X}, 0)`);
-        cloud2.setAttribute('transform', `translate(${cloud2X}, 0)`);
-        
+        cloud1.setAttribute('x', cloud1X);
+        cloud2.setAttribute('x', cloud2X);
+
         // Check if clouds have moved off-screen to the left
         if (cloud1X < -300) {
             cloud1X = 1920; // Reset to the right
         }
-        
+
         if (cloud2X < -300) {
             cloud2X = 1920; // Reset to the right
         }
-        
+
         // Request animation frame
         requestAnimationFrame(moveClouds);
     }
-    
+
     // Start the animation
     moveClouds();
 }
