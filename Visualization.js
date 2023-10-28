2bfccb08aa77c4cfa645611390794a2f8342c20f
audreyleighleigh <84555172+audreyleighleigh@users.noreply.github.com>
2023-10-28 15:34:16 -0600

    Add files via upload

--- Visualization.js
+++ Visualization.js
@@ -35,89 +35,34 @@ function animateClouds() {
         requestAnimationFrame(moveClouds);
     }
 
-    // Function to handle slider functionality
-function initSlider() {
-    const sliderBar = document.getElementById('XYBar'); // Updated slider bar ID
-    const sliderDot = document.getElementById('Dot1'); // Updated slider dot/thumb ID
-  
-    // Flag to track whether the user is currently dragging the slider
-    let isDragging = false;
-  
-    // Get the initial dot position from the SVG element
-    let dotX = parseFloat(sliderDot.getAttribute('cx'));
-  
-    // Function to update the dot's position
-    function updateDot() {
-      sliderDot.setAttribute('cx', dotX);
-    }
-  
-    // Event listener for mouse down on the slider bar
-    sliderBar.addEventListener('mousedown', (event) => {
-      isDragging = true;
-      console.log('Slider clicked'); // Log a message when the dot is clicked
-      updateDotPosition(event);
-    });
-  
-    // Event listener for mouse move (dragging) on the slider bar
-    sliderBar.addEventListener('mousemove', (event) => {
-      if (isDragging) {
-        updateDotPosition(event);
-      }
-    });
-  
-    // Event listener for mouse up (release) anywhere on the page
-    window.addEventListener('mouseup', () => {
-      isDragging = false;
-    });
-  
-    // Function to update the dot's position based on mouse coordinates
-    function updateDotPosition(event) {
-      const sliderRect = sliderBar.getBoundingClientRect();
-      const mouseX = event.clientX - sliderRect.left;
-  
-      // Ensure the dot stays within the slider bounds
-      dotX = Math.min(390, Math.max(10, mouseX));
-      updateDot();
-  
-      // Calculate a value based on the dot's position (you can adjust this formula)
-      const sliderValue = (dotX - 10) / 380;
-  
-      // Use the sliderValue as needed (e.g., for animations or other actions)
-      console.log('Slider Value:', sliderValue);
-    }
-  
-    // Initialize the dot's position
-    updateDot();
-  }
-  
-  // Run the slider initialization when the window loads
+    // Your existing JavaScript code
 
-  
+// Add a DOMContentLoaded event listener to ensure the code runs when the DOM is ready
+document.addEventListener("DOMContentLoaded", function () {
+  // Add drag behavior to the slider track (XYBar)
+  const sliderTrack = d3.select("#XYBar rect.slider-track");
 
-    // Start the animation
-    moveClouds();
-}
+  const drag = d3.drag().on("drag", function (event) {
+    // Update the position of the slider thumb based on user drag
+    const x = Math.max(288, Math.min(1755, event.x)); // Constrain within slider track
+    dot1.attr("transform", `translate(${x}, 176)`);
 
-// Function to initialize the element swapping
-function initializeElementSwapping() {
-  // Get references to the visible and hidden elements
-  const elementVisible = document.getElementById('Square5');
-  const elementHidden = document.getElementById('Square5Filled');
-
-  // Add a click event listener to the visible element
-  elementVisible.addEventListener('click', function() {
-    // Hide the visible element
-    elementVisible.style.display = 'none';
-
-    // Show the hidden element
-    elementHidden.style.display = 'inline'; // or 'block' depending on your layout needs
+    // Calculate the selected value based on the thumb's position
+    const selectedValue = (x - 288) / 1467; // Adjust as needed
+    // You can use the selectedValue to update other elements or perform actions.
   });
-}
 
+  sliderTrack.call(drag);
+});
+
+// Function to handle slider functionality
+ 
 // Run the initialization when the window loads
 
 window.onload = () => {
     animateClouds(); // Your existing animation
-    initSlider();    // Initialize the slider
-    initializeElementSwapping();
+    initSlider(); // Initialize the slider
   };
+
+
+  // Path: Visualization.js
